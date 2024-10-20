455ec7ffd519fe66808713cbcbf1e330ae1ccb4a
Manvir Singh Channa <manvirsingh098@gmail.com>
2024-10-20 10:25:06 +0530

    Update FeatureAnnouncement.js

--- src/FeatureAnnouncement.js
+++ src/FeatureAnnouncement.js
@@ -15,7 +15,7 @@ function FeatureAnnouncement() {
 
     // Set up WebSocket connection for new features
     useEffect(() => {
-        const ws = new WebSocket('ws://localhost:3001');
+        const ws = new WebSocket('wss://feature-announcement-backend.onrender.com/ws');
 
         ws.onmessage = (event) => {
             const feature = JSON.parse(event.data);
